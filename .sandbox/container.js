fe2036dca5922c81d109454a9d7403fd95e8e3a6
Jan Exner <jexner@adobe.com>
2018-10-27 14:54:49 +0200

    Added delay to initialisation, fixes

--- .sandbox/container.js
+++ .sandbox/container.js
@@ -1,36 +1,26 @@
 module.exports = {
-  "dataElements": {
-    "foo": {
-      "modulePath": "sandbox/localStorage.js",
-      "settings": {
-        "name": "foo"
-      },
-      "cleanText": true,
-      "defaultValue": "bar"
-    }
-  },
+  "dataElements": {},
   "rules": [{
-    "id": "RL1540473749765",
-    "name": "TestRule",
+    "id": "RL1540640627905",
+    "name": "Test 3",
     "events": [{
       "modulePath": "time-spent-and-scroll-depth-combined-event/src/lib/events/combinedTimeSpentAndScrollDepth.js",
       "settings": {
-        "timeSpent": "10",
+        "timeSpent": "15",
         "scrollDepth": "50",
-        "fireOnce": true
+        "fireOnce": true,
+        "fireOnShortPages": true
       }
     }]
   }],
   "extensions": {},
   "property": {
-    "settings": {
-      "domains": ["jan-exner.de"]
-    }
+    "settings": {}
   },
   "buildInfo": {
     "turbineVersion": "25.1.3",
-    "turbineBuildDate": "2018-10-25T13:22:31.932Z",
-    "buildDate": "2018-10-25T13:22:31.932Z",
+    "turbineBuildDate": "2018-10-27T11:43:53.182Z",
+    "buildDate": "2018-10-27T11:43:53.182Z",
     "environment": "development"
   }
 }
--- src/lib/events/combinedTimeSpentAndScrollDepth.js
+++ src/lib/events/combinedTimeSpentAndScrollDepth.js
@@ -3,6 +3,7 @@
 module.exports = function(settings, trigger) {
   var targetScrollDepth = settings.scrollDepth || 50; // percent of page height
   var targetTimeSpent = settings.timeSpent || 30; // in seconds
+  var initDelay = settings.initDelay || 100; // in milliseconds
 
   // keep track of state
   var state = {
@@ -25,38 +26,52 @@ module.exports = function(settings, trigger) {
     }
   }, targetTimeSpent * 1000);
 
-  try {
-    // listener 2 - scroll depth
-    var pageHeightInPixels = Math.max(
-      document.documentElement["clientHeight"],
-      document.body["scrollHeight"],
-      document.documentElement["scrollHeight"],
-      document.body["offsetHeight"],
-      document.documentElement["offsetHeight"]
-    );
-    var viewportHeightInPixels = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    if (viewportHeightInPixels >= pageHeightInPixels) {
-      // no scrolling on this short page
-      state.targetScrollDepthReached = settings.fireOnShortPages;
-    } else {
-      // create a listener for scrolling
-      var targetScrollDepthInPixels = (pageHeightInPixels - viewportHeightInPixels) * targetScrollDepth / 100;
-      window.addEventListener('scroll', function(e) {
-        try {
-          var currentScrollDepthInPixels = document.documentElement.scrollTop;
-          if (currentScrollDepthInPixels >= targetScrollDepthInPixels) {
-            state.targetScrollDepthReached = true;
-            if (state.targetTimeSpentReached) {
-              if (state.hasTriggered === false || settings.fireOnce === false) {
-                state.hasTriggered = true;
-                trigger({
-                  "subType": "scrollDepth"
-                });
+  // listener 2 - scroll depth
+  setTimeout( function() {
+    try {
+      var document = require('@adobe/reactor-document');
+      var h1 = 0, h2 = 0;
+      if ('undefined' !== typeof document.documentElement && document.documentElement) {
+        h1 = Math.max(
+          document.documentElement["clientHeight"],
+          document.documentElement["scrollHeight"],
+          document.documentElement["offsetHeight"]
+        );
+      }
+      if ('undefined' !== typeof document.body && document.body) {
+        h2 = Math.max(
+          document.body["scrollHeight"],
+          document.body["offsetHeight"]
+        );
+      }
+      var pageHeightInPixels = Math.max(h1, h2);
+      var window = require('@adobe/reactor-window');
+      var viewportHeightInPixels = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+      if (viewportHeightInPixels >= pageHeightInPixels) {
+        // no scrolling on this short page
+        state.targetScrollDepthReached = settings.fireOnShortPages;
+      } else {
+        // create a listener for scrolling
+        var targetScrollDepthInPixels = (pageHeightInPixels - viewportHeightInPixels) * targetScrollDepth / 100;
+        window.addEventListener('scroll', function(e) {
+          try {
+            var currentScrollDepthInPixels = document.documentElement.scrollTop;
+            if (currentScrollDepthInPixels >= targetScrollDepthInPixels) {
+              state.targetScrollDepthReached = true;
+              if (state.targetTimeSpentReached) {
+                if (state.hasTriggered === false || settings.fireOnce === false) {
+                  state.hasTriggered = true;
+                  trigger({
+                    "subType": "scrollDepth"
+                  });
+                }
               }
             }
+          } catch(e2) {
           }
-        } catch(e2) {}
-      });
+        });
+      }
+    } catch(e) {
     }
-  } catch(e) {}
+  }, initDelay);
 };
