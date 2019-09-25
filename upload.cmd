c1314f6eb7029584fada923d665843e774926b19
Jan Exner <jexner@adobe.com>
2019-09-25 15:35:00 +0200

    in which our hero adds good scripts

---
+++ upload.cmd
@@ -0,0 +1,2 @@
+@echo off
+npx @adobe/reactor-uploader --org-id=BE5121AA565821017F000101@AdobeOrg --tech-account-id=EC1D39425BD2C7FF0A495ECC@techacct.adobe.com --api-key=89546d44b3644128b24dc9ad68e1bb4e
